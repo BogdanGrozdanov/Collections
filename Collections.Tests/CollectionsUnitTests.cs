86efa52c91729d86dc3d6720a8c2aeedce03ab7d
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-05-15 22:04:59 +0300

    Add test

--- Collections.Tests/CollectionsUnitTests.cs
+++ Collections.Tests/CollectionsUnitTests.cs
@@ -1,18 +1,31 @@
 using NUnit.Framework;
+using System.Collections.ObjectModel;
 
 namespace Collections.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        //    [SetUp]
+        //    public void Setup()
+        //    {
+        //    }
+
+        [Test]
+        public void Test_Collection_EmptyConstructor_()
         {
+            Collection<int> emptyCollection = new Collection<int>();
+
+            Assert.That(emptyCollection.Count, Is.EqualTo(0), "Count property");
+            Assert.That(emptyCollection.Capacity, Is.EqualTo(16), "Count property");
+            Assert.That(emptyCollection.ToString(), Is.EqualTo("[]"), "Empty constructor not work");
         }
 
         [Test]
-        public void Test1()
+        public void Test_Collection_ConstructorSingleItem()
         {
-            Assert.Pass();
+            Collection<int> collection = new Collection<int>();
+
         }
+
     }
 }
