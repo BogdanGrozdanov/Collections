2db6a99ed80d9f4269a12559bcc751cfdea19cd0
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-05-16 18:38:23 +0300

    Add tests for Collection

--- Collections.Tests/CollectionsUnitTests.cs
+++ Collections.Tests/CollectionsUnitTests.cs
@@ -23,9 +23,49 @@ namespace Collections.Tests
         [Test]
         public void Test_Collection_ConstructorSingleItem()
         {
-            Collection<int> collection = new Collection<int>();
-
+            Collection<int> nums = new Collection<int>(7);
+            Assert.That(nums.ToString(), Is.EqualTo("[7]"));
         }
 
+        [Test]
+        public void Test_Collection_ConstructorMultipleItems()
+        {
+            Collection<int> nums = new Collection<int>(7, 8, 9);
+            Assert.That(nums.ToString(), Is.EqualTo("[7, 8, 9]"));
+        }
+
+        [Test]
+        public void Test_Collection_Add()
+        {
+            Collection<int> nums = new Collection<int>();
+            nums.Add(1);
+            nums.Add(2);
+            nums.Add(3);
+            Assert.That(nums.ToString(), Is.EqualTo("[1, 2, 3]"));
+        }
+
+        [Test]
+        public void Test_Collection_AddWithGrow()
+        {
+            Collection<int> nums = new Collection<int>();
+            var oldCapasity = nums.Capacity;
+
+            for (int i = 0; i <= oldCapasity; i++)
+            {
+                nums.Add(i);
+            }
+            var newCapacity = nums.Capacity;
+
+            Assert.IsTrue(newCapacity > oldCapasity, "Collection not grow - Capacity property");
+        }
+
+        [Test]
+        public void Test_Collection_AddRange()
+        {
+            Collection<int> nums = new Collection<int>(1, 2, 3);
+            nums.AddRange(4, 5, 6);
+            var expected = nums.ToString();
+            Assert.That(expected, Is.EqualTo("[1, 2, 3, 4, 5, 6]"));
+        }
     }
 }
